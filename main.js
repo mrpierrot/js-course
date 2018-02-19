7a81b4130b5a084e53a91651ce04b8f7e3c7af6d
Pierre Chabiland <pierrot@elpoyo.local>
2018-02-19 15:23:29 +0100

    create first page

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+var chokidar = require('chokidar');
 var asciidoctor = require('asciidoctor.js')();
 require('asciidoctor-reveal.js');
 
 // Convert the document 'presentation.adoc' using the reveal.js converter
-var attributes = {'revealjsdir': '../node_modules/reveal.js@'};
+var attributes = {'revealjsdir': '../../node_modules/reveal.js@'};
 var options = {safe: 'safe', backend: 'revealjs', attributes: attributes};
-asciidoctor.convertFile('./courses/course_01/index.adoc', options);
+
+
+chokidar.watch('courses/**/*.adoc', {ignored: /(^|[\/\\])\../}).on('all', (event, path) => {
+    switch(event){
+        case 'add':
+        case 'change':
+            asciidoctor.convertFile(path, options);
+            console.log(`${path} converted`);
+    }
+   
+});
