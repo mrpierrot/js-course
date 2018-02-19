cc54834dbe16ee41defaacb1b5990ae768e82583
Pierre Chabiland <pierrot@elpoyo.local>
2018-02-19 19:04:18 +0100

    creates all exercices for TP1

--- main.js
+++ main.js
@@ -4,15 +4,24 @@ require('asciidoctor-reveal.js');
 
 // Convert the document 'presentation.adoc' using the reveal.js converter
 var attributes = {'revealjsdir': '../../reveal.js@'};
-var options = {safe: 'safe', backend: 'revealjs', attributes: attributes};
-
+var slidesOptions = {safe: 'safe', backend: 'revealjs', attributes: attributes};
 
 chokidar.watch('courses/**/*.adoc', {ignored: /(^|[\/\\])\../}).on('all', (event, path) => {
     switch(event){
         case 'add':
         case 'change':
-            asciidoctor.convertFile(path, options);
+            asciidoctor.convertFile(path, slidesOptions);
             console.log(`${path} converted`);
     }
-   
+});
+
+var pageOptions = {safe: 'safe',doctype:'article', backend: 'html5'};
+
+chokidar.watch('exercices/**/*.adoc', {ignored: /(^|[\/\\])\../}).on('all', (event, path) => {
+    switch(event){
+        case 'add':
+        case 'change':
+            asciidoctor.convertFile(path,pageOptions);
+            console.log(`${path} converted`);
+    }
 });
