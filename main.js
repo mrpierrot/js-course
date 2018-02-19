d5a2b44237b5e9996adb121cee90ec7b298ecf35
Pierre Chabiland <pierrot@elpoyo.local>
2018-02-19 15:34:01 +0100

    add reveal.js on root (bad but this will bit fixed later)

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var asciidoctor = require('asciidoctor.js')();
 require('asciidoctor-reveal.js');
 
 // Convert the document 'presentation.adoc' using the reveal.js converter
-var attributes = {'revealjsdir': '../../node_modules/reveal.js@'};
+var attributes = {'revealjsdir': '../../reveal.js@'};
 var options = {safe: 'safe', backend: 'revealjs', attributes: attributes};
 
 
